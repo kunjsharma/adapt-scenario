a039fc8f0605379ec2b45dc51f8e4840643e7007
Kunj <kunjsharma@hotmail.com>
2019-08-08 13:25:52 +0530

    Edit files

--- js/adapt-scenario.js
+++ js/adapt-scenario.js
@@ -7,6 +7,7 @@ define(function(require) {
         events: {
             'click .scenario-navigator-button': 'navigateClick'
         },
+
         preRender: function() {
             //OUR HBS template requires this helper
             if (Handlebars.helpers && !('ifvalue' in Handlebars.helpers)) {
@@ -23,12 +24,10 @@ define(function(require) {
 
             this.listenTo(Adapt, 'device:resize', this.setDeviceSize, this);
             this.listenTo(Adapt, 'device:changed', this.setDeviceSize, this);
-            
-          
         },
+
         setDeviceSize: function() {
             if (Adapt.device.screenSize !== 'small') {
-                //if (!this.model.get('_isDesktop')) this.resetScenario();
                 this.showScenarioItems();
                 this.model.set('_isDesktop', true);
                 this.$el.removeClass('desktop mobile').addClass('desktop');
@@ -38,12 +37,14 @@ define(function(require) {
                 this.$el.removeClass('desktop mobile').addClass('mobile');
             }
         },
+
         postRender: function() {
             this.setupScenario();
             this.showScenarioItems();
             this.setReadyStatus();
             this.model.set('_itemsOnStage', this.model.get('itemsOnStage'));
         },
+
         setupScenario: function() {
             this.setDeviceSize();
             var slideCount = this.model.get('items').length;
@@ -51,15 +52,16 @@ define(function(require) {
             this.model.set('_active', true);
             this.updateScenarioProgressBar();
         },
+
         navigateClick: function(event) {
             event.preventDefault();
             if (!this.model.get('_active')) return;
-            var $target = this.$(event.currentTarget);
-            if ($target.hasClass('left')) {
+            var clickedButton = this.$(event.currentTarget);
+            if (clickedButton.hasClass('left')) {
                 if (this.model.get('_currentScenario') > 0) {
                     this.model.set('_currentScenario', this.model.get('_currentScenario') - 1);
                 } else return;
-            } else if ($target.hasClass('right') || $target.hasClass('down')) {
+            } else if (clickedButton.hasClass('right') || clickedButton.hasClass('down')) {
                 if (this.model.get('_currentScenario') >= (this.model.get('_itemCount'))) {
                     return;
                 } else {
@@ -73,40 +75,42 @@ define(function(require) {
             
             console.log("this.model.get('_currentScenario')", this.model.get('_currentScenario'));
         },
+
         showScenarioItems: function() {
             if (Adapt.device.screenSize !== 'small') {
                 var currentScenarioNo = this.model.get('_currentScenario') - 1,
-                $container = this.$('.scenario-contanier');
+                container = this.$('.scenario-contanier');
                 if (this.model.get('_currentScenario') == 0) {
-                    $container.addClass("hidden");
+                    container.addClass("hidden");
                 } else {
                     var itemCount = (currentScenarioNo) % (this.model.get('_itemsOnStage'));
                     var startIndex = 0;
                     var endIndex = 0;
-                    $container.addClass("hidden");
-                    if (itemCount == 0) $container.eq(currentScenarioNo).removeClass('hidden').css({'opacity': 0}).animate({opacity: 1});
+                    container.addClass("hidden");
+                    if (itemCount == 0) container.eq(currentScenarioNo).removeClass('hidden').css({'opacity': 0}).animate({opacity: 1});
                     else {
                         startIndex = Math.floor((currentScenarioNo) / (this.model.get('_itemsOnStage'))) * (this.model.get('_itemsOnStage'));
                         endIndex = currentScenarioNo;
                         for (var i = startIndex; i <= endIndex; i++) {
-                            $container.eq(i).removeClass('hidden');
+                            container.eq(i).removeClass('hidden');
                         }
                     }
                 }
             
-                $($container).eq(currentScenarioNo).a11y_focus();
+                $(container).eq(currentScenarioNo).a11y_focus();
             } else {
                 this.$('.scenario-contanier').removeClass('hidden');
                 this.setCompletionStatus();
             }
         },
+
         updateScenarioProgressBar: function() {
              var index = this.model.get('_currentScenario'),
                 total = this.model.get('_itemCount'),
-                _nWidth = (index / total) * 100;
-            //this.$('.scenario-navigator-progressbar-fill').width(_nWidth + '%');
+                width = (index / total) * 100;
+            //this.$('.scenario-navigator-progressbar-fill').width(width + '%');
             this.$('.scenario-navigator-progressbar-fill').animate({
-                width: (_nWidth + '%')
+                width: (width + '%')
             }, 100);
             this.$('.scenario-navigator-button').removeClass('disabled').attr('aria-hidden', false).prop('disabled', false);;
             if(index == 0) {
@@ -132,6 +136,7 @@ define(function(require) {
                 this.setCompletionStatus();
             }
         },
+
         resetScenario: function() {
             if (Adapt.device.screenSize !== 'small') {
                 this.model.set('_currentScenario', 0);
@@ -147,6 +152,7 @@ define(function(require) {
                 return item.visited;
             });
         },
+        
         evaluateCompletion: function() {
             if (this.getVisitedItems().length == this.model.get('items').length) {
                 this.setCompletionStatus();
